5f41f52f47f12a80d9c4241218ee95590e43a538
Hannes Erik Rother <hrother@c2r7p2.42vienna.com>
2023-11-25 15:53:50 +0100

    all norminette fixed for real this time

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 15:23:07 by hrother           #+#    #+#             */
-/*   Updated: 2023/11/06 18:49:28 by hrother          ###   ########.fr       */
+/*   Updated: 2023/11/25 15:42:39 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
