76f80537b6deb49ab1d32aa137ce3ea8ab02a315
Hannes Erik Rother <hrother@c2r7p2.42vienna.com>
2023-11-25 15:40:59 +0100

    more norminette

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 13:14:43 by hrother           #+#    #+#             */
-/*   Updated: 2023/11/25 14:48:14 by hrother          ###   ########.fr       */
+/*   Updated: 2023/11/25 15:40:25 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ typedef struct s_stack
 	int	*arr;
 	int	size;
 	int	max_size;
-	int offset;
+	int	offset;
 }	t_stack;
 
 typedef struct s_rotset
 {
-	int nra;
-	int nrra;
-	int nrb;
-	int nrrb;
+	int	nra;
+	int	nrra;
+	int	nrb;
+	int	nrrb;
 }	t_rotset;
 
 char		*exec_str(t_stack *stack_a, t_stack *stack_b, char *str);
 void		update_offset(t_stack *stack, int ascending);
 int			is_sorted(t_stack stack);
-int			init_stacks(int argc, char **argv, t_stack *stack_a, t_stack *stack_b);
+int			init_stacks(int c, char **argv, t_stack *stack_a, t_stack *stack_b);
 void		push_next_nbr(t_stack *src, t_stack *dest, int btoa);
 void		solve(t_stack *stack_a, t_stack *stack_b);
 void		ft_swap(int *a, int *b);
